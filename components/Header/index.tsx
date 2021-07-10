e22fde5a343fed4269cf19b25f42909c1b959214
Evgeniy Ponomarev <41052525+qxie11@users.noreply.github.com>
2021-07-10 20:56:18 +0300

    Mobile header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,23 +1,33 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import Logo from '../Logo';
 import Nav from '../Nav';
 import styles from './styles.module.scss';
+import classNames from 'classnames';
 
 const Header: React.FC = () => {
     const header = useRef<HTMLHeadElement | null>(null);
+    const [isNavActive, setNavActive] = useState<boolean>(false);
 
     useEffect(() => {
-        gsap.from(header.current, {y: -50, duration: 1.9, delay: 0.2, ease: 'elastic'})
-    })
+        gsap.from(header.current, {y: -50, duration: 1.9, delay: 0.2, ease: 'elastic'});
+    }, []);
 
     return (
-        <div className='container'>
-            <header ref={header} className={styles.header}>
+        <>
+            <header ref={header} className={classNames({
+                [styles.header]: true,
+                [styles.active]: isNavActive,
+            })}>
                 <Logo />
                 <Nav />
             </header>
-        </div>
+            <div className={styles.menuBtn} onClick={() => setNavActive(!isNavActive)}>
+                <span className={styles.menuBtnLine}></span>
+                <span className={styles.menuBtnLine}></span>
+                <span className={styles.menuBtnLine}></span>
+            </div>
+        </>
     )
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import { createContext } from 'react';
-import Header from "../components/Header";
-import HomeIntro from "../components/HomeIntro";
+import Head from 'next/head';
+import Header from '../components/Header';
+import HomeIntro from '../components/HomeIntro';
 
 export const NavDataContext = createContext(null);
 
 export default function Home({ data }) {
   return (
-    <>
+      <>  
+        <Head>
+          <title>Evgeniy Ponomarev</title>
+        </Head>
         <NavDataContext.Provider value={data.nav}>
             <Header />
         </NavDataContext.Provider>
