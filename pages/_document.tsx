14f74051e827804e72e86b666dcb90c8fe136dd5
Evgeniy Ponomarev <41052525+qxie11@users.noreply.github.com>
2021-08-09 19:02:31 +0300

    Add _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+          <meta name="theme-color" content="#b80929" />
+          <meta property="og:image" content="/images/me.jpg" />
+        </Head>
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
