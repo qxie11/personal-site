cc7ce8abb2024c00bc2190a7fe2b1e7f0b65011e
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2022-07-25 22:49:42 +0300

    add google ad

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
+  componentDidMount(): void {
+    window?.dataLayer = window?.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-185699919-1');
+  }
+
   render() {
     return (
       <Html>
@@ -11,6 +21,11 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-185699919-1"
+          ></script>
+          <script>{}</script>
         </body>
       </Html>
     );
